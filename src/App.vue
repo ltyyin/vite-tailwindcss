bfac72ea2c907ba291221db0abe9b709100a14d6
tyyin <2417040797@qq.com>
2023-06-19 01:05:25 +0800

    build: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  // console.log('hello')
+
+  console.log(window.ENV)
+})
+</script>
+
+<template>
+  <div>hello</div>
+</template>
+
+<style scoped></style>
